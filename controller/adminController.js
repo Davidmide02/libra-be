1a6241ef68e197977f6eb41dc8cbb899831fbb98
Davidmide02 <Davidmide07@gmail.com>
2024-08-06 16:56:25 +0100

    create

--- controller/adminController.js
+++ controller/adminController.js
@@ -3,25 +3,35 @@ exports.createMaterial = (req, res, next) => {
 
   const materialTitle = req.body.title;
   const author = req.body.author;
-  const iSNb = req.body.isnb;
+  const isbn = req.body.isnb;
+//   add available as defualt value and make it optional in database
+//   const trackBook = req.body.trackBook;
+  const category = req.body.category;
+
+  //   available||reserved||checkout||new
+
+  //   member subscription
+  // membership type
+  // renewal
 
   res
     .status(200)
-    .json({ message: "Material add successfully", material: "title" });
+    .json({
+      message: "Material add successfully",
+      material: "title",
+      materialTitle,
+      author,
+    });
 };
 
-exports.editMaterial = (req,res,next) =>{
-    // get the id of the materials to be editted
-    // check if the materials exist in the Db
-    // get the updates and save to the database
+exports.editMaterial = (req, res, next) => {
+  // get the id of the materials to be editted
+  // check if the materials exist in the Db
+  // get the updates and save to the database
+};
 
-}
-
-
-
-exports.deleteMaterial = (req,res,next) =>{
-    // get the id of the materials to be editted
-    // check if the materials exist in the Db
-    // delete the material
-
-}
+exports.deleteMaterial = (req, res, next) => {
+  // get the id of the materials to be editted
+  // check if the materials exist in the Db
+  // delete the material
+};
