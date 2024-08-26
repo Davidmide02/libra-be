6b5e507fe2bafc21c64cbc0aa42385782168b704
Davidmide02 <Davidmide07@gmail.com>
2024-08-26 16:51:49 +0100

    env file

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 const adminRoutes = require("./routes/admin");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
+const db_username = process.env.DB_NAME;
+const db_password = process.env.DB_PASSWORD;
 
 app.use(bodyParser.json());
 
@@ -30,11 +32,10 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://davidmide07:MCxxoXAGCyATrXW3@lmscluster.37roy.mongodb.net/?retryWrites=true&w=majority&appName=lmsCluster"
+    `mongodb+srv://${db_username}:${db_username}@lmscluster.37roy.mongodb.net/?retryWrites=true&w=majority&appName=lmsCluster`
   )
   .then(console.log("connected to databasee"), app.listen(8080))
   .catch((err) => {
     console.log("Cannot connect to database");
     console.log(err);
-
   });
