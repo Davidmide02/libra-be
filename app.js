041bd3a4b5c8eed5e3a75278cc717048a77668bd
David Olamide Adenusi <99823023+Davidmide02@users.noreply.github.com>
2024-10-01 18:28:10 +0100

    localhost added
    for cors error

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const db_password = process.env.DB_PASSWORD;
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
