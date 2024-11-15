5ef7e9b9234f54c79bf67d69134559f2c0c08202
Davidmide02 <Davidmide07@gmail.com>
2024-11-15 16:36:19 +0100

    cors package

--- app.js
+++ app.js
@@ -7,18 +7,25 @@ const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const adminAuthMiddleware = require("./middleware/adminAuthmiddleware");
 const userAuthMiddleware = require("./middleware/userAuthMiddleware");
+const cors = require("cors");
 const db_username = process.env.DB_USERNAME;
 const db_password = process.env.DB_PASSWORD;
 
 app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+    credentials: true,
+  })
+);
 
-app.use((req, res, next) => {
-
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+//   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 
 app.use("/admin", adminAuthMiddleware, adminRoutes);
 app.use("/user", userAuthMiddleware, userRoutes);
@@ -44,4 +51,4 @@ mongoose
     console.log("Cannot connect to database");
     // console.log(err.message);
   });
-``
+``;
