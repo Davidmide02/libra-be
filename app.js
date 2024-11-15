dcb315514a98f36930eb06f66cb7bc6e34b1e438
Davidmide02 <Davidmide07@gmail.com>
2024-11-15 13:22:49 +0100

    domain localhost

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
 
-  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.5173");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
